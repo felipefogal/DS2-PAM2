66ece23f7a97d69d63c6aa6809c7e392ab09e0cd
Felipe Fogal <felipefogal@gmail.com>
2025-03-17 22:26:13 -0300

    Primeiro commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Feather from 'react-native-vector-icons/Feather';
+
+import Home from './src/pages/Home/index';
+import Profile from './src/pages/Profile/index';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ tabBarShowLabel: true }}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ color, size }) => {
+              return <Feather name="home" color={'tomato'} size={size} />;
+            },
+          }}
+        />
+
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarIcon: ({ color, size }) => {
+              return <Feather name="user" color={'tomato'} size={size} />;
+            },
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
